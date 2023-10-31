ad38add71536e7b1cb0a5ca6c09126d1f0189021
jeevanalexen <86559072+castlercodes@users.noreply.github.com>
2023-10-31 22:55:23 +0530

    new changes

--- app/page.js
+++ app/page.js
@@ -1,10 +1,19 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import React from 'react';
+import './page.css';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push('/find');
+  }
   return (
-    <main className={styles.main}>
-      
+    <main className='main'>
+      <div className='titleContainer'>
+        <h1 className='title'>Welcome to Codeforces Mashup Creator</h1>
+      </div>
+      <button onClick={handleClick}>Find Problems for your mashup</button>
     </main>
-  )
+  );
 }
